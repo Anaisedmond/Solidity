3d8d6e82ec276e03b48e998db19f773f753f3ed2
Anaisedmond <Anaisedmond@gmail.com>
2024-06-25 17:14:46 +0400

    Updated Proposal

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
+    uint256 private counter; // This line is added
 
     struct Proposal {
         string title; // Title of the proposal
@@ -10,10 +11,23 @@ contract ProposalContract {
         uint256 reject; // Number of reject votes
         uint256 pass; // Number of pass votes
         uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
+        bool current_state; // This shows the current state of the proposal, meaning whether it passes or fails
         bool is_active; // This shows if others can vote to our contract
     }
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
+    function create(string calldata _title, string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal({
+            title: _title,
+            description: _description,
+            approve: 0,
+            reject: 0,
+            pass: 0,
+            total_vote_to_end: _total_vote_to_end,
+            current_state: false,
+            is_active: true
+        });
+    }
 }
